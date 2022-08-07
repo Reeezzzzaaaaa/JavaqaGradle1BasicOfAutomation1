3283fd0fc13a2d592c3b4c0b3810271db5b52c26
АлександÑAleksandr Ryazantsev <amapkymap@gmail.com>
2022-08-07 20:45:36 +0300

    testng commit

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,45 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void shouldRemain999Test() {
+
+        CashbackHackService service = new CashbackHackService();
+        int amount = 999;
+
+        int actual = service.remain(amount);
+        int expected = 1;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldRemain1000Test() {
+
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldRemain1001Test() {
+
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1001;
+
+        int actual = service.remain(amount);
+        int expected = 999;
+
+        assertEquals(actual, expected);
+    }
+
+}
