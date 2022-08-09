54702416355afa4b6300cb5c84d34495c16282cf
АлександÑAleksandr Ryazantsev <amapkymap@gmail.com>
2022-08-09 21:43:54 +0300

    testng commit

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -19,6 +19,18 @@ public class CashbackHackServiceTest {
     }
 
     @Test
+    public void shouldRemain1000Test() {
+
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
     public void shouldRemain1001Test() {
 
         CashbackHackService service = new CashbackHackService();
@@ -32,6 +44,7 @@ public class CashbackHackServiceTest {
 
     @Test
     public void shouldZeroTest() {
+
         CashbackHackService service = new CashbackHackService();
         int amount = 0;
 
@@ -41,17 +54,6 @@ public class CashbackHackServiceTest {
         assertEquals(actual, expected);
     }
 
-    @Test
-    public void shouldRemain1000Test() {
-
-        CashbackHackService service = new CashbackHackService();
-        int amount = 1000;
-
-        int actual = service.remain(amount);
-        int expected = 0;
-
-        assertEquals(actual, expected);
-    }
 
     @Test
     public void shouldUnderZeroTest() {
@@ -59,7 +61,7 @@ public class CashbackHackServiceTest {
         int amount = -1;
 
         int actual = service.remain(amount);
-        int expected = 1001;
+        int expected = -1;
 
         assertEquals(actual, expected);
     }
