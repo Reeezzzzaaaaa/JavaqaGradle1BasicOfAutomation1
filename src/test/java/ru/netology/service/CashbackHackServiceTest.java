5362b3b07b1d537ae478f8e88c8180c5cfb81689
АлександÑAleksandr Ryazantsev <amapkymap@gmail.com>
2022-08-09 21:45:56 +0300

    junit4 commit

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -19,6 +19,18 @@ public class CashbackHackServiceTest {
     }
 
     @Test
+    public void shouldRemain1000Test() {
+
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 1000;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
     public void shouldRemain1001Test() {
 
         CashbackHackService service = new CashbackHackService();
@@ -38,18 +50,6 @@ public class CashbackHackServiceTest {
 
         int actual = service.remain(amount);
         int expected = 1000;
-
-        assertEquals(actual, expected);
-    }
-
-    @Test
-    public void shouldRemain1000Test() {
-
-        CashbackHackService service = new CashbackHackService();
-        int amount = 1000;
-
-        int actual = service.remain(amount);
-        int expected = 0;
 
         assertEquals(actual, expected);
     }
